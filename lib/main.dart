6b6cb5c1b7ac543801b7094ba693082201a6304c
Diogo Sequeira <diogosequeira@hotmail.fr>
2020-09-14 18:53:03 +0100

    added first list screen and form screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:car_dealer_service/screens/places_form_screen.dart';
+import 'package:car_dealer_service/screens/places_list_screen.dart';
+import 'package:car_dealer_service/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,58 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'BMW Car Dealer Places',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PlacesListScreen(),
+      routes: {
+        AppRoutes.PLACE_FORM: (ctx) => PlaceFormScreen(),
+      }
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,23 @@
+import 'package:car_dealer_service/utils/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Car Locations'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: (){
+                Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+              },)
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
