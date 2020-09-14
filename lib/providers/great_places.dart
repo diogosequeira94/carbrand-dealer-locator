5e8c53a6a0e1eeeeeacc6bf69d4fa72878e9b34e
Diogo Sequeira <diogosequeira@hotmail.fr>
2020-09-14 19:01:57 +0100

    added provider

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,18 @@
+import 'package:car_dealer_service/models/place.dart';
+import 'package:flutter/material.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  Place itemByIndex(int index) {
+    return _items[index];
+  }
+}
